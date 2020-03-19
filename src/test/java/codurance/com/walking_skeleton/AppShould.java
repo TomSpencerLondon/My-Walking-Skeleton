761574a791f4a4989222bc844f3eae3f9fe21bee
Tom Spencer <tom@homeflow.co.uk>
2020-03-19 10:25:30 +0000

    Correct AppShould

---
+++ src/test/java/codurance/com/walking_skeleton/AppShould.java
@@ -0,0 +1,12 @@
+package codurance.com.walking_skeleton;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppShould {
+  @Test
+  void true_is_true() {
+    assertTrue(true);
+  }
+}
